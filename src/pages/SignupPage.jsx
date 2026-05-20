92ac6e8aea1d057321eaaa3a52b13ef735b141ec
seoyn95 <seoyn95@hufs.ac.kr>
2026-05-20 20:31:57 +0900

    https 수정

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -20,7 +20,7 @@ function SignupPage() {
     e.preventDefault();
     try {
       await axios.post(
-        "http://kikoky.duckdns.org:8000/accounts/register/",
+        "https://kikoky.duckdns.org:8000/accounts/register/",
         { login_id: id, name: username, password, email, role, department: dept }
       );
       
